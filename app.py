04d3b714b568977c5dc4d7b6f8226a5542fcd197
Josiah Nyamai <mungutijosiah1@gmail.com>
2024-05-08 12:01:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ st.header('Car Price Prediction Machine Learning Model')
 cars_data = pd.read_csv('Cardetails.csv')
 
 st.text("""
-        This web app allows a user to predict the prices of a car based on their manufacture 
-        year, number of kilometers driven, fuel type, transmission, seller type, mileage,
-        number of seats, engine size and horse power parameters.
+        This web app allows a user to predict the prices of a car based on their 
+        manufacturer year, number of kilometers driven, fuel type, transmission,
+        seller type, mileage,number of seats, engine size and horse power parameters.
         """
     )
 
